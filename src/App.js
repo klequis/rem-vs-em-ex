fa93a900323fb46899f78667447da4047ce66a90
klequis <klequis@gmail.com>
2019-02-15 07:30:09 -0800

    unknow

--- src/App.js
+++ src/App.js
@@ -1,28 +1,45 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import injectSheet from 'react-jss'
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+const App = ({ classes }) => {
+  return (
+    <div className={classes.wrapper}>
+      <div className={classes.divPx}>
+        <h1 className={classes.h1Px}>Heading 1 (px)</h1>
       </div>
-    );
-  }
+      <div className={classes.divEm}>
+        <h1 className={classes.h1Em}>Heading 1 (em)</h1>
+      </div>
+      <div className={classes.divRem}>
+        <h1 className={classes.h1Rem}>Heading 1 (rem)</h1>
+      </div>
+    </div>
+  )
 }
 
-export default App;
+const styles = {
+  wrapper: {
+    backgroundColor: '#282c34',
+    minHeight: '100vh',
+  },
+  divPx: {
+    fontSize: '18px'
+  },
+  h1Px: {
+    fontSize: '32px',
+  },
+  divEm: {
+    fontSize: '18px'
+  },
+  h1Em: {
+    fontSize: '2em'
+  },
+  divRem: {
+    fontSize: '18px'
+  },
+  h1Rem: {
+    fontSize: '2rem'
+  },
+}
+
+export default injectSheet(styles)(App)
